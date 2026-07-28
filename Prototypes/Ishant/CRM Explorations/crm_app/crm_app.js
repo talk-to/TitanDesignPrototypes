18b0984e593147ef8f9346ea06f8e5f71dbb4ee5
ishant <ishantp@titan.email>
2026-07-28 11:36:58 +0530

    Add Associated Company data card to Overview tab right after Personal Notes, establishing the Person -> Company relationship

--- Prototypes/Ishant/CRM Explorations/crm_app/crm_app.js
+++ Prototypes/Ishant/CRM Explorations/crm_app/crm_app.js
@@ -81,6 +81,8 @@ const CrmApp = (function () {
       if (titleEl) titleEl.textContent = 'All Notes';
     } else if (viewId === 'more-details') {
       if (titleEl) titleEl.textContent = 'Contact Details';
+    } else if (viewId === 'company-details') {
+      if (titleEl) titleEl.textContent = 'Company Profile';
     } else if (viewId === 'all-activity') {
       if (titleEl) titleEl.textContent = 'All Activity';
     } else {
